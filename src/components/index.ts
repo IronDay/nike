bf5b27c40b5dfbd3ba7bca3eea5aa1995be9b822
Djioleu Florian <nsiegningflorian@gmail.com>
2023-08-26 23:41:13 +0100

    Build the Services section

--- src/components/index.ts
+++ src/components/index.ts
@@ -2,5 +2,6 @@ import Button from "./Button.tsx";
 import NavBar from "./NavBar.tsx";
 import ShoeCard from "./ShoeCard.tsx";
 import PopularProductCard from "./PopularProductCard.tsx";
+import ServiceCard from "./ServiceCard.tsx";
 
-export { Button, NavBar, ShoeCard, PopularProductCard };
+export { Button, ServiceCard, NavBar, ShoeCard, PopularProductCard };
