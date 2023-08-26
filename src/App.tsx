a9ff4a07385aecd594540099956758fe4fb7db89
Djioleu Florian <nsiegningflorian@gmail.com>
2023-08-26 14:41:11 +0100

    Setting up the app structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,48 @@
-import {useEffect} from "react";
+import {
+  Hero,
+  PopularProducts,
+  SuperQuality,
+  Services,
+  SpecialOffers,
+  CustomerReviews,
+  Subscribe,
+  Footer,
+} from "./sections";
+import { useEffect } from "react";
 
 function App() {
-    useEffect(() => {document.title = "Nike: dress as you like"}, []);
-    return (
-        <h1 className="text-3xl font-bold underline">
-            Hello World
-        </h1>
-    );
+  useEffect(() => {
+    document.title = "Nike: dress as you like";
+  }, []);
+  return (
+    <main className="relative ">
+      <header>Nav</header>
+      <section className="xl:padding-b wide:padding-r padding-b">
+        <Hero />
+      </section>
+      <section className="padding">
+        <PopularProducts />
+      </section>
+      <section className="padding">
+        <SuperQuality />
+      </section>
+      <section className="padding-x py-10">
+        <Services />
+      </section>
+      <section className="padding">
+        <SpecialOffers />
+      </section>
+      <section className="bg-pale-blue padding">
+        <CustomerReviews />
+      </section>
+      <section className="padding-x sm:py-32 py-16 w-full">
+        <Subscribe />
+      </section>
+      <section className="bg-black padding-x padding-t pb-8">
+        <Footer />
+      </section>
+    </main>
+  );
 }
 
 export default App;
---
+++ src/sections/index.ts
@@ -0,0 +1,19 @@
+import CustomerReviews from "./CustomerReviews.tsx";
+import Footer from "./Footer.tsx";
+import Hero from "./Hero.tsx";
+import PopularProducts from "./PopularProducts.tsx";
+import Services from "./Services.tsx";
+import SpecialOffers from "./SpecialOffers.tsx";
+import Subscribe from "./Subscribe.tsx";
+import SuperQuality from "./SuperQuality.tsx";
+
+export {
+  CustomerReviews,
+  Footer,
+  Subscribe,
+  SuperQuality,
+  Hero,
+  PopularProducts,
+  Services,
+  SpecialOffers,
+};
